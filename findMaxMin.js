f9f0a82391a9bfc44898d77725a4431bea36b7d1
raihan tajdid <raihantajdid@gmail.com>
2023-06-05 22:23:02 +0600

    Find Min and Max value

---
+++ findMaxMin.js
@@ -0,0 +1,24 @@
+/* 
+Question:  Find the largest and smallest number in an unsorted array of integers
+Return: object — containing value of min and max 
+*/
+
+const arr = [1, 2, 3, 4, 100];
+const findMaxMin = (arr) => {
+    let max = arr[0];
+    let min = arr[0];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        } else if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return {
+        "max": max,
+        "min": min
+    };
+}
+console.log(findMaxMin(arr)); // Returns object { "max": 100, "min": 1 }
+
