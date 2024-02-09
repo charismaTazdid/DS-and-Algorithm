b1491b4069d38841244e87d0f461247f3e9fdd16
raihantazdid <raihantajdid@gmail.com>
2024-02-09 23:01:20 +0600

    DFS in javascript

---
+++ dfs.js
@@ -0,0 +1,25 @@
+function dfs(node) {
+    if (!node) {
+        return [];
+    }
+
+    const stack = [node];
+    const result = [];
+
+    while (stack.length > 0) {
+        const current = stack.pop();
+        result.push(current.value);
+
+        if (current.right) {
+            stack.push(current.right);
+        }
+
+        if (current.left) {
+            stack.push(current.left);
+        }
+    }
+
+    return result;
+}
+
+dfs(tree); // [1, 2, 4, 5, 3, 6, 7]
